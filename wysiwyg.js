1362102d40c463e15f644f2c61a8f556fa40317c
jevonthomas <jevon.g.thomas@gmail.com>
2017-06-27 11:43:31 -0500

    created 2 person objects in javascript, structured the html page in the basic style that i want

---
+++ wysiwyg.js
@@ -0,0 +1,21 @@
+{
+  title: "Actor",
+  name: "Bruce Lee",
+  bio: "Lee Jun-fan, known professionally as Bruce Lee, was a Hong Kong and American actor, film director, martial artist, philosopher and founder of the martial art Jeet Kune Do. Lee was the son of Cantonese opera star Lee Hoi-chuen.",
+  image: "https://en.wikipedia.org/wiki/Bruce_Lee#/media/File:Bruce_Lee_1973.jpg",
+  lifespan: {
+    birth: 1940,
+    death: 1973
+  }
+}
+
+{
+  title: "Professional Boxer",
+  name: "Muhammad Ali",
+  bio: "Muhammad Ali was an American professional boxer and activist. He is widely regarded as one of the most significant and celebrated sports figures of the 20th century.",
+  image: "https://en.wikipedia.org/wiki/Muhammad_Ali#/media/File:Muhammad_Ali_NYWTS.jpg",
+  lifespan: {
+    birth: 1942,
+    death: 2016
+  }
+}
