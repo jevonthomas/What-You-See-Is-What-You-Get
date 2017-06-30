1668a6db8ef7029819d9e4bf82779717afc38d44
jevonthomas <jevon.g.thomas@gmail.com>
2017-06-29 22:16:15 -0500

    set the event listeners to the text bar and person cards. set the person cards to dynaically be inserted into the DOM.

--- wysiwyg.js
+++ wysiwyg.js
@@ -23,3 +23,31 @@ let person = [
 }
 ]
 
+for (let i = 0; i < person.length; i++) {
+  document.getElementsByClassName("container")[0].innerHTML +=
+      `<article class="person-card">
+        <div class="profile">
+          ${person.image} width="200px" height="200px" alt="profile photo">
+          <header><h2>${person.name} | ${person.title}</h2></header>
+          <footer>${person.lifespan} - ${person.lifespan}</footer>
+        </div>
+        <div class="bio">
+          <section>${person.bio}</section>
+        </div>
+      </article>`
+}
+
+// When you click on one of the person elements, the text input should immediately gain focus so that you can start typing.
+let personCard = document.getElementsByClassName("person-card");
+let inputBar = document.getElementById("search-bar");
+for (let i = 0; i < personCard.length; i++) {
+  //add event listener for person card click to bring the input bar into focus
+  personCard[i].addEventListener("click", function() {
+    inputBar.focus();
+    //add keyup event for input bar
+    inputBar.addEventListener("keyup", function(){
+      //input value will appear in .bio section element
+      event.target.nextSibling.nextSibling.nextSibling.nextSibling.childNodes[3].childNodes[3].childNodes[1].innerHTML = document.getElementById("search-bar").value;
+    })
+  })
+}
